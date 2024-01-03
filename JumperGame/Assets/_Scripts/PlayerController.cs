f2186eb33f0f74862a5028758ba3834ebd382fe3
Miras Bolatbekuly <mynameismiras2003@gmail.com>
2024-01-03 12:32:51 +0300

    Added particle effects and sounds

--- JumperGame/Assets/_Scripts/PlayerController.cs
+++ JumperGame/Assets/_Scripts/PlayerController.cs
@@ -2,6 +2,17 @@ using UnityEngine;
 
 public sealed class PlayerController : MonoBehaviour
 {
+    [Space(10)]
+    [Header("Particles")]
+    [SerializeField] private ParticleSystem explosionParticle;
+    [SerializeField] private ParticleSystem dirtSplatter;
+
+    [Space(20)]
+    [Header("AudioClips")]
+    [SerializeField] private AudioClip crashSound;
+    [SerializeField] private AudioClip jumpSound;
+
+    private AudioSource audioPlayer;
     private Rigidbody rb;
     private Animator anim;
     private Death death;
@@ -12,6 +23,7 @@ public sealed class PlayerController : MonoBehaviour
 
     private void Awake()
     {
+        audioPlayer = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         death = Death.Instance;
@@ -22,13 +34,22 @@ public sealed class PlayerController : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         Collider other = collision.collider;
-        if (other.CompareTag("Ground"))
-            isOnGround = true;
-        else if (other.CompareTag("Obstacle"))
+        if (!isGameOver)
         {
-            isGameOver = true;
-            AnimateDeath();
-            death.NotifyObservers();
+            if (other.CompareTag("Ground"))
+            {
+                isOnGround = true;
+                dirtSplatter.Play();
+            }
+            else if (other.CompareTag("Obstacle"))
+            {
+                isGameOver = true;
+                audioPlayer.PlayOneShot(crashSound, 0.7f);
+                AnimateDeath();
+                dirtSplatter.Stop();
+                explosionParticle.Play();
+                death.NotifyObservers();
+            }
         }
     }
 
@@ -36,6 +57,7 @@ public sealed class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !isGameOver)
         {
+            dirtSplatter.Stop();
             Jump();
             isOnGround = !isOnGround;
         }
@@ -45,6 +67,7 @@ public sealed class PlayerController : MonoBehaviour
     {
         anim.SetTrigger("Jump_trig");
         rb.AddForce(Vector3.up * forceAmount, ForceMode.Impulse);
+        audioPlayer.PlayOneShot(jumpSound, 1);
     }
 
     private void AnimateDeath()
